89a754ee5f812327ff664bcd107f9ce95ba79779
rafaFabian07 <facegamers343@gmail.com>
2026-02-23 11:12:50 +0700

    ku tambahin fungsi ascending

--- tugas1.cpp
+++ tugas1.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void ascending() {// rafa
-    cout << "halo";// 
+void ascending(int array[], int kapsitas) {
+    int temp;
+    for (int i = 0; i < kapsitas - 1; i++)// loop luar => 
+    {
+        for (int j = 0; j < kapsitas - i - 1; j++)// loop dalam
+        {
+            if (array[j] > array[j + 1])// kondisi
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+            
+        }
+        
+    }
+    
 }
 void descending() {// diaz
     cout << "halo";
@@ -15,7 +30,15 @@ void keluar(){// keluar
 }
 
 int main(){
-    
+    int nilai[10] = {75, 60, 85, 90, 70, 88, 65, 80, 93, 62};
+    int size = 10;
+
+    ascending(nilai, size);
+    cout << "ascending : ";
+    for (int elemen : nilai)
+    {
+        cout << elemen << " ";
+    }
     
     
     return 0;
