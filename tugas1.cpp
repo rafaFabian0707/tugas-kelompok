d196cbfcec89f4827f14fdb2ca3f2724655b8fb7
rafaFabian07 <facegamers343@gmail.com>
2026-02-23 10:47:18 +0700

    first commit

---
+++ tugas1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void ascending() {// rafa
+    cout << "halo";// 
+}
+void descending() {// diaz
+    cout << "halo";
+}
+void search() {// nayla
+    cout << "halo";
+}
+void keluar(){// keluar
+
+}
+
+int main(){
+    
+    
+    
+    return 0;
+}
